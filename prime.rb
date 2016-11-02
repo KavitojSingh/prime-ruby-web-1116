eeaf20f8f01915b9327e753e711ddd0d1c3474e2
Kavitoj Singh <kavitojsingh@gmail.com>
2016-11-02 02:58:53 +0000

    Prime Ruby

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(number)
+  if number < 2
+    return false
+  end
+  for factor in 2...number
+    if number % factor == 0
+      return false
+    end
+  end
+  true
+end
